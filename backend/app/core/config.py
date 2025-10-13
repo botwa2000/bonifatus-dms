de3d6c2aa5fefdfbf9ea813230269a90ca9dd7d3
botwa2000 <alexanderperel@gmail.com>
2025-10-13 19:32:39 +0200

    fix config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,7 @@
 import logging
 import os
 from functools import lru_cache
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel, Field
 from pydantic_settings import BaseSettings
 
